f313498ccd830066edc3a0ad1189f2b1984b65fb
wael963 <waelbouraoui0@gmail.com>
2024-03-19 13:20:45 +0100

    express

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+
+const authmiddleware = (req, res, next) => {
+    const auth = false
+    const currentDay = new Date().getDay()
+    const currentHour = new Date().getHours()
+    console.log(currentDay, currentHour)
+    if (currentDay >= 4 && currentDay <= 5 && currentHour >= 9 && currentHour <= 17) {
+        next()
+    }
+    else {
+        res.send("you are not autorized")
+    }
+}
+app.use(authmiddleware)
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/view/home.html')
+})
+app.get("/contact", (req, res) => {
+    res.sendFile(__dirname + '/view/contact.html')
+})
+app.get("/ourService", (req, res) => {
+    res.sendFile(__dirname + '/view/ourService.html')
+})
+app.listen(8081, (err) => {
+    err ? console.log('err:', err) : console.log('Server is running in port 80811')
+})
